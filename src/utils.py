989ef047810f71623084bf2ee5fbe60587ffa242
David Pollack <david.pollack@yeay.com>
2018-06-07 13:53:46 +0000

    code changes for reflection padding and miro example

--- src/utils.py
+++ src/utils.py
@@ -12,13 +12,26 @@ def scale_img(style_path, style_scale):
     style_target = _get_img(style_path, img_size=new_shape)
     return style_target
 
-def get_img(src, img_size=False):
+def get_img(src, img_size=None):
    img = scipy.misc.imread(src, mode='RGB') # misc.imresize(, (256, 256, 3))
    if not (len(img.shape) == 3 and img.shape[2] == 3):
        img = np.dstack((img,img,img))
-   if img_size != False:
+   if img_size:
        img = scipy.misc.imresize(img, img_size)
    return img
+
+def get_img_eval(src, img_size=None, border=0.05):
+   img = get_img(src, img_size)
+   if border:
+       xb, yb  = tuple([int(s * border) for s in img.shape[:2]])
+       img = scipy.pad(img, ((xb, xb), (yb, yb), (0,0)), mode='reflect')
+   else:
+       xb = yb = 0
+   return img, (xb, yb)
+
+def crop_img(img, borders):
+   xb, yb = borders
+   return img[xb:-xb, yb:-yb, :]
 
 def exists(p, msg):
     assert os.path.exists(p), msg
